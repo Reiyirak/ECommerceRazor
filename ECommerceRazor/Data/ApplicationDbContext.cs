19d0210179792d477d500941a0dabd661c01cfce
Reiyirak <pdsmej@gmail.com>
2025-05-20 17:17:58 -0600

    Setup the project
    Create new Razor Pages Web Application, then configure the Db Connection
    String, create the ApplicationDbContext contructor, and add the
    DbContext to the builder.services in the Program.cs file.

---
+++ ECommerceRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceRazor.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        // Aqui van a agregarse los DbSets de los modelos(tablas) mas adelante
+    }
+}
