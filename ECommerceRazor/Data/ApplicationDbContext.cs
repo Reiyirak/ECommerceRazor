51ee7ea0bac429ab081140198e78e19edccc39f4
Reiyirak <pdsmej@gmail.com>
2025-05-22 13:38:52 -0600

    Create the initial migration for Category Model
    I used the command "dotnet ef migrations add <migration-name>" to create
    the inital migration after adding the DbSet of the Category Model to the
    ApplicationDbContext file.
    Then I used the command "dotnet ef database update" to create the
    Categories table with it's properties.

--- ECommerceRazor/Data/ApplicationDbContext.cs
+++ ECommerceRazor/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using System;
+using ECommerceRazor.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceRazor.Data;
@@ -7,6 +8,8 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        // Aqui van a agregarse los DbSets de los modelos(tablas) mas adelante
     }
+
+    // Aqui van a agregarse los DbSets de los modelos(tablas) mas adelante
+    public DbSet<Category> Categories { get; set; }
 }
