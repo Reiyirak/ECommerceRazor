9692d0a94f483c5ddd613d8afca1387071747fe1
Reiyirak <pdsmej@gmail.com>
2025-05-28 12:52:23 -0600

    Create generic interface

---
+++ ECommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ECommerce.DataAccess.Repository.IRepository;
+
+public interface IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        // Metodos para CRUD. Leer registros, registro individual
+        // Crear registro, Borrar, Borrar Masivo
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
