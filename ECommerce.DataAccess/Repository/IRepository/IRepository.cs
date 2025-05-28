e2f3fc19a38b7ad509491464dcc56065c34a3007
Reiyirak <pdsmej@gmail.com>
2025-05-28 13:11:24 -0600

    Implement generic interface

--- ECommerce.DataAccess/Repository/IRepository/IRepository.cs
+++ ECommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -2,16 +2,13 @@
 
 namespace ECommerce.DataAccess.Repository.IRepository;
 
-public interface IRepository
+public interface IRepository<T> where T : class
 {
-    public interface IRepository<T> where T : class
-    {
-        // Metodos para CRUD. Leer registros, registro individual
-        // Crear registro, Borrar, Borrar Masivo
-        void Add(T entity);
-        void Remove(T entity);
-        void RemoveRange(IEnumerable<T> entity);
-        IEnumerable<T> GetAll();
-        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
-    }
+    // Metodos para CRUD. Leer registros, registro individual
+    // Crear registro, Borrar, Borrar Masivo
+    void Add(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entity);
+    IEnumerable<T> GetAll();
+    T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
 }
